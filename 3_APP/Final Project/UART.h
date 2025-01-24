4f354aaddd0a5145f9cf815a1f805fd1c2a8a8f3
Karem1heikal <147204895+Karem1heikal@users.noreply.github.com>
2025-01-24 02:03:57 +0200

    Delete 3_APP/Final Project directory

--- 3_APP/Final Project/UART.h
+++
@@ -1,9 +0,0 @@
-#ifndef UART_COMMUNICATION
-#define UART_COMMUNICATION
-
-void UART_Init();
-void UART_SendChar(char c);
-char UART_ReceiveChar();
-void UART_SendString(char *str);
-
-#endif //UART_COMMUNICATION
--- 3_APP/Final Project/config.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONFIGUATION
-#define CONFIGUATION
-
-#define MAX_BUFFER 32 // Buffer size for UART communication
-#define SECURITY_LEVEL 5 // Security level for Fermat's primality test
-
-#endif //CONFIGUATION
